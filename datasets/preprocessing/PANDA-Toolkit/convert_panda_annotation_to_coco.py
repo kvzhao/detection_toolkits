9bd68e4582e9b09e68042c430d7730bb9d33c700
kvzhao <kelispinor@gmail.com>
2020-05-04 18:59:40 +0800

    Add converter for panda source annotations

---
+++ datasets/preprocessing/PANDA-Toolkit/convert_panda_annotation_to_coco.py
@@ -0,0 +1,197 @@
+import os
+from os.path import join
+import sys
+import json
+from pprint import pprint
+
+from collections import defaultdict
+
+
+from pycocotools.coco import COCO
+
+def load_json(path):
+    with open(path, 'r') as fp:
+        jsonfile = json.load(fp)
+    return jsonfile
+
+CATEGORY = {
+    'visible body': 1,
+    'full body': 2,
+    'head': 3,
+    'vehicle': 4
+}
+
+CATMAP = {
+    'fbox': 'full body',
+    'vbox': 'visible body',
+    'hbox': 'head',
+    'vehicle': 'vehicle'
+}
+
+def restrain_between_0_1(values_list):
+    return_list = []
+    for value in values_list:
+        if value < 0:
+            new_value = 0
+        elif value > 1:
+            new_value = 1
+        else:
+            new_value = value
+        return_list.append(new_value)
+    return return_list
+
+def RectDict2List(rectdict, imgwidth, imgheight, scale, mode='tlbr'):
+    x1, y1, x2, y2 = restrain_between_0_1([rectdict['tl']['x'], rectdict['tl']['y'],
+                                           rectdict['br']['x'], rectdict['br']['y']])
+    xmin = int(x1 * imgwidth * scale)
+    ymin = int(y1 * imgheight * scale)
+    xmax = int(x2 * imgwidth * scale)
+    ymax = int(y2 * imgheight * scale)
+
+    if mode == 'tlbr':
+        return xmin, ymin, xmax, ymax
+    elif mode == 'tlwh':
+        return xmin, ymin, xmax - xmin, ymax - ymin
+
+def main(args):
+    output_dir = args.output_dir
+    panda_annotation_path = args.panda_annotation_path
+
+    anno_types = args.selected_annotation_types
+
+    print(anno_types)
+
+    panda_annos = load_json(panda_annotation_path)
+
+    os.makedirs(output_dir, exist_ok=True)
+
+    json_dict = {
+        'images': [],
+        'annotations': [],
+        'categories': [
+            {
+            'supercategory': 'person',
+            'id': CATEGORY[CATMAP[anno]],
+            'name': CATMAP[anno],
+            } for anno in anno_types
+        ],
+    }
+
+    bbox_id = 1
+    ignore = 0 
+
+    scene_ids = defaultdict(list)
+
+    # per splitted image
+    # TODO: Aggregate same large image or same scenario
+    for imagename, imagedict in panda_annos.items():
+        imgwidth = imagedict['image size']['width']
+        imgheight = imagedict['image size']['height']
+        image_id = imagedict['image id']
+        scale = 1.0
+
+        scenename = imagename.split('/IMG')[0]
+        scene_ids[scenename].append(image_id)
+
+        #scale = showwidth / imgwidth
+        image_info = {
+            'file_name': imagename,
+            'height': imgheight,
+            'width': imgwidth,
+            'id': image_id,
+        }
+        json_dict['images'].append(image_info)
+
+        # per annotation
+        for object_dict in imagedict['objects list']:
+            objcate = object_dict['category']
+            if objcate == 'person':
+                #personpose = object_dict['riding type'] if object_dict['pose'] == 'riding' else object_dict['pose']
+                if 'fbox' in anno_types:
+                    fullrect = RectDict2List(object_dict['rects']['full body'], imgwidth, imgheight, scale, 'tlwh')
+                    annotation = {
+                            'area': fullrect[2] * fullrect[3],
+                            'iscrowd': ignore,
+                            'image_id': image_id,
+                            'bbox': fullrect,
+                            'category_id': CATEGORY[CATMAP['fbox']],
+                            'id': bbox_id,
+                            'ignore': ignore,
+                            'segmentation': [],
+                    }
+                    json_dict['annotations'].append(annotation)
+                    bbox_id += 1
+
+                if 'vbox' in anno_types:
+                    visiblerect = RectDict2List(object_dict['rects']['visible body'], imgwidth, imgheight, scale, 'tlwh')
+                    annotation = {
+                            'area': visiblerect[2] * visiblerect[3],
+                            'iscrowd': ignore,
+                            'image_id': image_id,
+                            'bbox': visiblerect,
+                            'category_id': CATEGORY[CATMAP['vbox']],
+                            'id': bbox_id,
+                            'ignore': ignore,
+                            'segmentation': [],
+                    }
+                    json_dict['annotations'].append(annotation)
+                    bbox_id += 1
+
+                if 'hbox' in anno_types:
+                    headrect = RectDict2List(object_dict['rects']['head'], imgwidth, imgheight, scale, 'tlwh')
+                    annotation = {
+                            'area': headrect[2] * headrect[3],
+                            'iscrowd': ignore,
+                            'image_id': image_id,
+                            'bbox': headrect,
+                            'category_id': CATEGORY[CATMAP['hbox']],
+                            'id': bbox_id,
+                            'ignore': ignore,
+                            'segmentation': [],
+                    }
+                    json_dict['annotations'].append(annotation)
+                    bbox_id += 1
+
+    output_path = join(output_dir, 'coco_anno.json')
+    with open(output_path, 'w') as json_fp:
+        json_str = json.dumps(json_dict)
+        json_fp.write(json_str)
+    print('Done, save to {}'.format(output_path))
+
+    # seperate into scenes and single images
+    coco = COCO(output_path)
+    scene_output_path = join(output_dir, 'scenes')
+    os.makedirs(scene_output_path, exist_ok=True)
+
+    for name, ids in scene_ids.items():
+        imgInfo = coco.loadImgs(ids)
+        annoInfo = coco.loadAnns(coco.getAnnIds(ids))
+        catInfo = [
+            {
+            'supercategory': 'person',
+            'id': CATEGORY[CATMAP[anno]],
+            'name': CATMAP[anno],
+            } for anno in anno_types
+        ]
+        json_dict = {
+            'images': imgInfo,
+            'annotations': annoInfo,
+            'categories': catInfo,
+        }
+        output_path = join(scene_output_path, name) + '.json'
+        with open(output_path, 'w') as json_fp:
+            json_str = json.dumps(json_dict)
+            json_fp.write(json_str)
+
+    print('Done.')
+
+if __name__ == '__main__':
+
+    import argparse
+    parser = argparse.ArgumentParser(description='Convert split.json to coco annotation format.')
+    parser.add_argument('-od', '--output_dir', type=str, default=None, help='')
+    parser.add_argument('-gt', '--panda_annotation_path', type=str, default=None, help='')
+    parser.add_argument('-at', '--selected_annotation_types', type=str, nargs='+', default=['fbox'],
+        help='Choose annotation to export: fbox, vbox, hbox, vehicle')
+    args = parser.parse_args()
+    main(args)
---
+++ datasets/preprocessing/PANDA-Toolkit/merge_coco_annotations.py
@@ -0,0 +1,166 @@
+import os
+import numpy as np
+import panda_utils as util
+import json
+from collections import defaultdict
+
+from pycocotools.coco import COCO
+
+CATID = {
+    1: 'vbox',
+    2: 'fbox',
+    3: 'hbox',
+}
+
+def recttransfer(rect, scale, left, up):
+    xmin, ymin, w, h = rect
+    xmax, ymax = xmin + w, ymin + h
+    return [int(temp / scale) for temp in [xmin + left, ymin + up, xmax + left, ymax + up]]
+
+def py_cpu_nms(dets, thresh):
+    #print('dets:', dets)
+    x1 = dets[:, 0]
+    y1 = dets[:, 1]
+    x2 = dets[:, 2]
+    y2 = dets[:, 3]
+    scores = dets[:, 4]
+
+    areas = (x2 - x1 + 1) * (y2 - y1 + 1)
+    ## index for dets
+    order = scores.argsort()[::-1]
+
+    keep = []
+    while order.size > 0:
+        i = order[0]
+        keep.append(i)
+        xx1 = np.maximum(x1[i], x1[order[1:]])
+        yy1 = np.maximum(y1[i], y1[order[1:]])
+        xx2 = np.minimum(x2[i], x2[order[1:]])
+        yy2 = np.minimum(y2[i], y2[order[1:]])
+
+        w = np.maximum(0.0, xx2 - xx1 + 1)
+        h = np.maximum(0.0, yy2 - yy1 + 1)
+        inter = w * h
+        ovr = inter / (areas[i] + areas[order[1:]] - inter)
+
+        inds = np.where(ovr <= thresh)[0]
+        order = order[inds + 1]
+
+    return keep
+
+
+def main(args):
+
+    srcannopath = args.src_annotation_path
+    split_annotation_path = args.split_annotation_path
+    detection_result_path = args.detection_result_path
+
+    is_nms = args.use_nms
+    output_dir = args.output_dir
+
+    os.makedirs(output_dir, exist_ok=True)
+
+    print('Loading source annotation json file: {}'.format(srcannopath))
+
+    coco = COCO(split_annotation_path)
+
+    mergedresults = defaultdict(list)
+    mergedinfos = defaultdict(dict)
+
+    # Source Annotation
+    with open(srcannopath, 'r') as load_f:
+        srcanno = json.load(load_f)
+
+    with open(detection_result_path, 'r') as load_f:
+        detresults = json.load(load_f)
+
+    detIndexed = defaultdict(list)
+    for det in detresults:
+        detIndexed[det['image_id']].append(
+            {'bbox': det['bbox'],
+            'category_id': det['category_id'],
+            'score': det['score']
+            }
+        )
+
+    for image_id, annoInfo in detIndexed.items():
+
+        imageInfo = coco.loadImgs(image_id)[0]
+        #annoInfo = coco.loadAnns(coco.getAnnIds(image_id))
+
+        filename = imageInfo['file_name']
+
+        srcfile, paras = filename.split('___')
+        srcfile = srcfile.replace('_IMG', '/IMG') + '.jpg'
+        scale, left, up = paras.replace('.jpg', '').split('__')
+
+        srcimageid = srcanno[srcfile]['image id']
+
+        if srcimageid not in mergedinfos:
+            mergedinfos[srcimageid].update(srcanno[srcfile]['image size'])
+            mergedinfos[srcimageid].update({'file_name': srcfile, 'id': srcimageid})
+
+        for objdict in annoInfo:
+            #score = objdict.get('score', 1.0)
+            mergedresults[srcimageid].append(
+                [*recttransfer(objdict['bbox'], float(scale), int(left), int(up)),
+                      objdict['score'], objdict['category_id']])
+
+    if is_nms:
+        for (imageid, objlist) in mergedresults.items():
+            keep = py_cpu_nms(np.array(objlist), args.nms_thresh)
+            outdets = []
+            for index in keep:
+                outdets.append(objlist[index])
+            mergedresults[imageid] = outdets
+
+    json_dict = {
+        'images': [],
+        'annotations': [],
+        'categories': [],
+    }
+
+    catset = set()
+    for (imageid, objlist) in mergedresults.items():
+        json_dict['images'].append(
+            mergedinfos[imageid]
+        )
+        for obj in objlist:
+            det = {
+                "image_id": imageid,
+                "category_id": obj[5],
+                "bbox": obj[:4],
+                "score": obj[4]
+            }
+            json_dict['annotations'].append(det)
+            catset.add(obj[5])
+    for cat in catset:
+        json_dict['categories'].append({
+            'supercategory': 'person',
+            'id': cat,
+            'name': CATID[cat]
+        })
+
+    output_path = os.path.join(output_dir, 'predictions.json')
+    with open(output_path, 'w') as json_fp:
+        json_str = json.dumps(json_dict)
+        json_fp.write(json_str)
+    print('Done, save to {}'.format(output_path))
+
+    output_path = os.path.join(output_dir, 'detres.json')
+    with open(output_path, 'w') as json_fp:
+        json_str = json.dumps(json_dict['annotations'])
+        json_fp.write(json_str)
+    print('Done, save to {}'.format(output_path))
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('-sp', '--split_annotation_path', type=str, default=None, help='Input COCO file')
+    parser.add_argument('-gt', '--src_annotation_path', type=str, default=None, help='PANDA Original Annotation')
+    parser.add_argument('-dt', '--detection_result_path', type=str, default=None, help='Detectuin results json')
+    parser.add_argument('-od', '--output_dir', type=str, default=None, help='')
+    parser.add_argument('--use_nms', action='store_true', help='')
+    parser.add_argument('--nms_thresh', type=float, default=0.5)
+    args = parser.parse_args()
+    main(args)
