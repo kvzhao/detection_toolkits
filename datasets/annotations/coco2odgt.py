6a098c6b4ab8d7abd9de092731a44664d626efd1
kvzhao <kelispinor@gmail.com>
2020-05-05 22:46:53 +0800

    Load detres

--- datasets/annotations/coco2odgt.py
+++ datasets/annotations/coco2odgt.py
@@ -11,7 +11,8 @@ import json
 # "gtboxes": [{"fbox": [72, 202, 163, 503], "tag": "person", "hbox": [171, 208, 62, 83], "extra": {"box_id": 0, "occ": 0}, "vbox": [72, 202, 163, 398], "head_attr": {"ignore": 0, "occ": 0, "unsure": 0}}, {"fbox": [199, 180, 144, 499], "tag": "person", "hbox": [268, 183, 60, 83], "extra": {"box_id": 1, "occ": 0}, "vbox": [199, 180, 144, 420], "head_attr": {"ignore": 0, "occ": 0, "unsure": 0}}, {"fbox": [310, 200, 162, 497], "tag": "person", "hbox": [363, 219, 54, 71], "extra": {"box_id": 2, "occ": 0}, "vbox": [310, 200, 162, 400], "head_attr": {"ignore": 0, "occ": 0, "unsure": 0}}, {"fbox": [417, 182, 139, 518], "tag": "person", "hbox": [455, 190, 53, 78], "extra": {"box_id": 3, "occ": 0}, "vbox": [417, 182, 139, 418], "head_attr": {"ignore": 0, "occ": 0, "unsure": 0}}, {"fbox": [429, 171, 224, 511], "tag": "person", "hbox": [537, 187, 55, 73], "extra": {"box_id": 4, "occ": 1}, "vbox": [534, 171, 113, 431], "head_attr": {"ignore": 0, "occ": 0, "unsure": 0}}, {"fbox": [543, 178, 262, 570], "tag": "person", "hbox": [602, 186, 71, 93], "extra": {"box_id": 5, "occ": 0}, "vbox": [543, 178, 257, 422], "head_attr": {"ignore": 0, "occ": 0, "unsure": 0}}]}
 
 def main(args):
-    df = dftools.from_coco(args.groundtruth_jsonfile_path)
+    df = dftools.from_coco(args.groundtruth_jsonfile_path,
+                           args.detection_result_path)
 
     print(df.keys())
 
@@ -50,6 +51,7 @@ if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('-gt', '--groundtruth_jsonfile_path', type=str, default=None)
+    parser.add_argument('-dt', '--detection_result_path', type=str, default=None)
     parser.add_argument('-od', '--output_dir', type=str, default=None)
     parser.add_argument('-bt', '--bbox_type', type=str, default='fbox')
     parser.add_argument('-at', '--annotation_type', type=str, default='dtboxes')
